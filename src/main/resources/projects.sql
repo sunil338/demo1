7b087b07213ab6c641ce331374e9a0b4b84f4fda
adikarthik <33541222+adikarthik@users.noreply.github.com>
2024-11-14 04:07:07 +0000

    Update projects.sql

--- src/main/resources/projects.sql
+++ src/main/resources/projects.sql
@@ -3,5 +3,5 @@ INSERT INTO PROJECT ( ID, NAME, DESCRIPTION)
 VALUES
 (1, 'Azure',	'Azure Project'),
 (2, 'Jenkins',	'Jenkins Project'),
-(3, 'Kubernetes',	'Kubernetes Project');
+(3, 'Kubernetes',	'Kubernetes Project'),
 (4, 'devops',	'devops Project');
